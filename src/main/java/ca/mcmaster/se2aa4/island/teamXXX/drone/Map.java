9faa9ba88f14f2a6a6c234d16ee4f302a60f4fe4
udaysinghkanwar <udaykanwar007@gmail.com>
2025-03-24 13:47:38 -0400

    Added more functionality to map class

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/drone/Map.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/drone/Map.java
@@ -64,6 +64,79 @@ public class Map {
                position.getY()>=0 || position.getY()<height;
     }
 
+    public boolean isAtMapBoundary(Position pos) {
+        return pos.getX() == 0 || pos.getX() == width - 1 ||
+               pos.getY() == 0 || pos.getY() == height - 1;
+    }
+
+
+    public Position getStartPosition() {
+        return startPosition;
+    }
+
+    public void setStartingPosition(Position pos) {
+        this.startPosition = pos;
+        addPosition(pos);
+    }
+
+    public boolean isAtSartPoistion(Position pos) {
+        return pos.equals(startPosition);
+    }
+
+    public List<POIType> getCreeks() {
+        return creeks;
+    }
+
+    public POIType getSite() {
+        return site;
+    }
+    
+    private void addCreek(POIType creek) {
+        creeks.add(creek);
+    }
+
+    private void addSite(POIType site) {
+        this.site = site;
+    }
+    
+    public void addPointOfInterest(POIType type) {
+        if (type == POIType.CREEK) {
+            addCreek(type);
+        } else if (type == POIType.EMERGENCY_SITE) {
+            addSite(type);
+        }
+    }
+
+    public boolean hasCreeks() {
+        return !creeks.isEmpty();
+    }
+
+    public boolean hasSite() {
+        return site != null;
+    }
+
+    public POIType getClosestCreek() {
+        if (!hasCreeks()) {
+            return null;
+        }
+        if (!hasSite()) {
+            return null;
+        }
+        POIType closestCreek = null;
+        double minDistance = -1;
+
+        for (POIType creek : creeks) {
+            double distance = creek.position().distanceTo(site.position());
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestCreek = creek;
+            }
+        }
+        return closestCreek;
+    
+    }
+    
+
 
     
     
