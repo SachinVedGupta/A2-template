7497f0becd79066d04e0ea4b170395d1da98ba5d
udaysinghkanwar <udaykanwar007@gmail.com>
2025-03-24 02:30:04 -0400

    fixed create request for RadarCommand

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/command/RadarCommand.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/command/RadarCommand.java
@@ -1,9 +1,18 @@
 package ca.mcmaster.se2aa4.island.teamXXX.command;
 
 import org.json.JSONObject;
+
 import ca.mcmaster.se2aa4.island.teamXXX.*;
+import eu.ace_design.island.game.actions.Heading;
 
 public class RadarCommand implements Command {
+    
+    private Heading heading;
+    
+    public RadarCommand(Heading heading) {
+        this.heading = heading;
+    }
+    
     @Override
     public CommandOption getCommandType() {
         return CommandOption.RADAR;
@@ -17,7 +26,10 @@ public class RadarCommand implements Command {
     @Override
     public JSONObject createRequestJSON() {
         JSONObject request = new JSONObject();
-        request.put("command", CommandOption.RADAR);
+        request.put("action", "echo");
+        JSONObject parameters = new JSONObject();
+        parameters.put("direction", heading.toString());
+        request.put("parameters", parameters);
         return request;
     }
 
