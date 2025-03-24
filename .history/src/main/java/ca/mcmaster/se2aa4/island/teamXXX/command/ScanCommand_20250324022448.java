2655988b7bdeac5e942f492ad5eebe580fc55259
SachinVedGupta <sachinvgupta9@gmail.com>
2025-03-24 02:29:03 -0400

    created the scan command

---
+++ .history/src/main/java/ca/mcmaster/se2aa4/island/teamXXX/command/ScanCommand_20250324022448.java
@@ -0,0 +1,23 @@
+package ca.mcmaster.se2aa4.island.teamXXX.command;
+import org.json.JSONObject;
+import ca.mcmaster.se2aa4.island.teamXXX.*;
+
+public class ScanCommand implements Command {
+    @Override
+    public CommandOption getCommandType() {
+        return CommandOption.SCAN;
+    }
+
+    @Override
+    public void applyCommandResult(Drone drone, CommandResult result) {
+        drone.decreaseBattery(result.getCost());        
+    }
+
+    @Override
+    public JSONObject createRequestJSON() {
+        JSONObject request = new JSONObject();
+        request.put("command", "scan");
+        return request;
+    }
+  
+}
